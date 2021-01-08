1a57afa0784fa218c3f4f85a84d38191f0922f02
Ciruy <ciruy.victory@gmail.com>
2021-01-08 18:21:01 +0800

    almost finish onionView

--- app/src/main/java/com/example/learnkt/ui/DownloadProgressActivity.kt
+++ app/src/main/java/com/example/learnkt/ui/DownloadProgressActivity.kt
@@ -14,35 +14,17 @@ class DownloadProgressActivity : BaseDisposableActivity() {
     override fun layout(): Int = R.layout.activity_main
 
     override fun initListeners() {
-//        helloWorld.bind(APIClient.instances.instanceRetrofit(Constant.THUNDER_DOWNLOAD_URL_BASE, WanAndroidAPI::class.java).downloadThunderDmgSize22M())
-//                .invoke(Consumer {
-//
+//        addDisposable(helloWorld.bind2ProgressDownload(thunder_download())
+//                .subscribe {
+//                    if (TextUtils.isEmpty(it.first)) helloWorld.text = "下载进度：${it.second}%"
+//                    else ToastUtil.long(this, it.first)
 //                })
-
-        addDisposable(helloWorld.bind2ProgressDownload(thunder_download())
-                .subscribe {
-                    if (TextUtils.isEmpty(it.first)) helloWorld.text = "下载进度：${it.second}%"
-                    else ToastUtil.long(this, it.first)
-                })
 
         helloWorld.download(NetConstant.firm_download(), Consumer {
             if (TextUtils.isEmpty(it.first)) helloWorld.text = "下载进度：${it.second}%"
             else ToastUtil.long(this, it.first)
         })
-
-//        addDisposable(helloWorld.onion<TextView, Flowable<ResponseBody>, Flowable<Pair<String, Int>>> { pair ->
-//            pair.first.bind2ProgressDownload(pair.second)
-//        }.invoke {
-//            APIClient.instances.instanceRetrofit(Constant.THUNDER_DOWNLOAD_URL_BASE, WanAndroidAPI::class.java).downloadThunderDmgSize22M()
-//        }.subscribe {
-//            if (TextUtils.isEmpty(it.first)) helloWorld.text = "下载进度：${it.second}%"
-//            else ToastUtil.long(this, it.first)
-//        })
-
-
     }
-
-
 }
 
 
