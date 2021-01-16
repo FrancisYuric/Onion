7727564ce757da23cad95cee31859e808cba5504
Ciruy B.Heimerdinger <ciruy.victory@gmail.com>
2021-01-16 14:00:42 +0800

    finish navigation.xml

--- app/src/main/java/com/example/learnkt/ui/fragment/CollectFragment.kt
+++ app/src/main/java/com/example/learnkt/ui/fragment/CollectFragment.kt
@@ -3,6 +3,6 @@ package com.example.learnkt.ui.fragment
 import com.example.learnkt.R
 import com.example.learnkt.ui.baseFragment.BaseFragment
 
-class CollectFragment :BaseFragment(){
+class CollectFragment : BaseFragment() {
     override fun layoutId(): Int = R.layout.frag_collect
 }
