0c57b15e6b6cb24fcc4b70719b542bc7e7ee07eb
Ciruy B.Heimerdinger <ciruy.victory@gmail.com>
2021-01-16 14:02:35 +0800

    create downloadProgressActivity.xml

--- app/src/main/java/com/example/learnkt/ui/DownloadProgressActivity.kt
+++ app/src/main/java/com/example/learnkt/ui/DownloadProgressActivity.kt
@@ -8,14 +8,14 @@ import com.example.learnkt.bean.NetConstant
 import com.example.learnkt.ui.baseActivity.BaseDisposableActivity
 import com.example.learnkt.util.ToastUtil
 import io.reactivex.functions.Consumer
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_download_progress.*
 
 @BuilderClass
 class DownloadProgressActivity : BaseDisposableActivity(), ISubBuilder<SubDownloadProgressActivity> {
     override
     fun subBuilder() = SubDownloadProgressActivity.__create(this)
 
-    override fun layout(): Int = R.layout.activity_main
+    override fun layout(): Int = R.layout.activity_download_progress
 
     override fun initListeners() {
         helloWorld.download(NetConstant.thunder_download(), Consumer {
