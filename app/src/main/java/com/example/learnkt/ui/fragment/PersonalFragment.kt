20a853872df85c8a032f41444cb18615bfd58769
Ciruy B.Heimerdinger <ciruy.victory@gmail.com>
2021-01-16 13:55:38 +0800

    create Home,Collect,Personal Fragment layout

--- app/src/main/java/com/example/learnkt/ui/fragment/PersonalFragment.kt
+++ app/src/main/java/com/example/learnkt/ui/fragment/PersonalFragment.kt
@@ -1,7 +1,8 @@
 package com.example.learnkt.ui.fragment
 
+import com.example.learnkt.R
 import com.example.learnkt.ui.baseFragment.BaseFragment
 
 class PersonalFragment :BaseFragment(){
-    override fun layoutId(): Int = 0
+    override fun layoutId(): Int = R.layout.frag_personal
 }
