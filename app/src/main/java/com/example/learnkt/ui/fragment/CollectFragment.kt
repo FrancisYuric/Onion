c9b7afd7eb8c625d9b74e0975ba9cc91937f8477
Ciruy B.Heimerdinger <ciruy.victory@gmail.com>
2021-01-16 13:56:41 +0800

    init navigation fragment

--- app/src/main/java/com/example/learnkt/ui/fragment/CollectFragment.kt
+++ app/src/main/java/com/example/learnkt/ui/fragment/CollectFragment.kt
@@ -1,7 +1,8 @@
 package com.example.learnkt.ui.fragment
 
+import com.example.learnkt.R
 import com.example.learnkt.ui.baseFragment.BaseFragment
 
 class CollectFragment :BaseFragment(){
-    override fun layoutId(): Int = 0
+    override fun layoutId(): Int = R.layout.frag_collect
 }
--- app/src/main/java/com/example/learnkt/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/learnkt/ui/fragment/HomeFragment.kt
@@ -1,7 +1,8 @@
 package com.example.learnkt.ui.fragment
 
+import com.example.learnkt.R
 import com.example.learnkt.ui.baseFragment.BaseFragment
 
 class HomeFragment :BaseFragment(){
-    override fun layoutId(): Int = 0
+    override fun layoutId(): Int = R.layout.frag_home
 }
