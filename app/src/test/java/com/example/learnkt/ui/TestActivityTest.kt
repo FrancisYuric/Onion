6c2cbd8cf660fadd173c146a48fffc76191005f9
Ciruy <ciruy.victory@gmail.com>
2021-01-21 15:19:47 +0800

    常见的单元测试已经完成，接下来是网络连接的相关测试

--- app/src/test/java/com/example/learnkt/ui/TestActivityTest.kt
+++ app/src/test/java/com/example/learnkt/ui/TestActivityTest.kt
@@ -1,9 +1,12 @@
 package com.example.learnkt.ui
 
+import com.example.learnkt.CiruyApplication
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(application = CiruyApplication::class)
 class TestActivityTest {
 
 }
