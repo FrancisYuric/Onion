2f54a5ec9710e61d3bc9bfda398dad7a7e63edc2
Ciruy <ciruy.victory@gmail.com>
2021-01-21 10:58:58 +0800

    UrlPattern test

--- app/src/test/java/com/example/learnkt/bean/UrlPatternTest.kt
+++ app/src/test/java/com/example/learnkt/bean/UrlPatternTest.kt
@@ -3,14 +3,13 @@ package com.example.learnkt.bean
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class ConstantTest {
+class UrlPatternTest {
     @Test
     fun testUrlPattern() {
         assertEquals(true, legalUrl("http://192.168.90.98"))
         assertEquals(false, legalUrl("192.168.0.98"))
         assertEquals(false, legalUrl("atp:192.168.0.98"))
-
-        //ip地址只写三位时会出问题
+        //ip地址只写三位时会出问题,该正则表达式并没有对于ip的位数做出限制
         assertEquals(false, legalUrl("http://192.168.0"))
     }
 }
