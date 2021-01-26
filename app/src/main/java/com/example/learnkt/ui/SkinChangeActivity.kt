7e916fa485013a1af51443d68db9ce5ebc925ae9
Ciruy <ciruy.victory@gmail.com>
2021-01-26 13:51:17 +0800

    add change skin plugin

---
+++ app/src/main/java/com/example/learnkt/ui/SkinChangeActivity.kt
@@ -0,0 +1,10 @@
+package com.example.learnkt.ui
+
+import com.ciruy.b.heimerdinger.onion_view.activity.BaseActivity
+import com.example.learnkt.R
+import com.example.learnkt.ui.baseActivity.BaseDisposableActivity
+
+class SkinChangeActivity :BaseDisposableActivity(){
+    override fun layout(): Int = R.layout.activity_skin_change
+    
+}
