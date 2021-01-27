ce6c080d1966f143dc687b9a50291a49a887e2c7
Ciruy <ciruy.victory@gmail.com>
2021-01-27 13:35:32 +0800

    create TestSkinActivity

---
+++ app/src/main/java/com/example/learnkt/ui/TestSkinPluginActivity.kt
@@ -0,0 +1,9 @@
+package com.example.learnkt.ui
+
+import com.example.learnkt.R
+import com.example.learnkt.ui.baseActivity.BaseDisposableActivity
+
+class TestSkinPluginActivity : BaseDisposableActivity() {
+    override fun layout(): Int = R.layout.activity_test_skin_plugin
+
+}
