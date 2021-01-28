539bbb1a493288bed5f8b6af71321e8cef75bc08
Ciruy <ciruy.victory@gmail.com>
2021-01-28 11:11:14 +0800

    skin change finish

--- app/src/main/java/com/example/learnkt/bean/ResultEntityWrapper.kt
+++ app/src/main/java/com/example/learnkt/bean/ResultEntityWrapper.kt
@@ -5,3 +5,4 @@ data class ResultEntityWrapper<T>(
     val errorCode: Int,
     val errorMsg: String?
 )
+
---
+++ app/src/main/java/com/example/learnkt/ext/ContextExt.kt
@@ -0,0 +1,12 @@
+package com.example.learnkt.ext
+
+import android.content.Context
+import android.text.TextUtils
+import com.ciruy.onion_base.util.ToastUtil
+import com.example.learnkt.bean.BaseEntity
+import com.example.learnkt.bean.ResultEntityWrapper
+
+fun <T : Context, F : ResultEntityWrapper<M>, M : BaseEntity> T.result(resultEntity: F) {
+    if (TextUtils.isEmpty(resultEntity.errorMsg)) ToastUtil.short(this, "登录成功！")
+    else ToastUtil.short(this, resultEntity.errorMsg)
+}
