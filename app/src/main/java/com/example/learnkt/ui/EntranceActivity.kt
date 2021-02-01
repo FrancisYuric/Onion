9578d233d87dcfbaae09a6ec650849e896bce597
Ciruy B.Heimerdinger <ciruy.victory@gmail.com>
2021-02-01 18:10:39 +0800

    create entrance activity

---
+++ app/src/main/java/com/example/learnkt/ui/EntranceActivity.kt
@@ -0,0 +1,8 @@
+package com.example.learnkt.ui
+
+import com.example.learnkt.R
+import com.example.learnkt.ui.baseActivity.BaseDisposableActivity
+
+class EntranceActivity : BaseDisposableActivity() {
+    override fun layout() = R.layout.activity_entrance
+}
