8dbcf7de33303a7605f31be7214b741aeb30b69b
Ciruy B.Heimerdinger <ciruy.victory@gmail.com>
2021-02-04 15:40:59 +0800

    onFalse

--- onion_base/src/main/java/com/ciruy/onion_base/rx/func.kt
+++ onion_base/src/main/java/com/ciruy/onion_base/rx/func.kt
@@ -4,3 +4,8 @@ fun Boolean.onTrue(trueConsumer: (Boolean) -> Unit): Boolean {
     if (this) trueConsumer.invoke(this)
     return this
 }
+
+fun Boolean.onFalse(falseConsumer: (Boolean) -> Unit): Boolean {
+    if (!this) falseConsumer.invoke(this)
+    return this
+}
