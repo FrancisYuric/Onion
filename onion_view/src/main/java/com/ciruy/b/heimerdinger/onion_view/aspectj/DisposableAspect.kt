f46153a9f50299fcb21af567847c622eb3d1487e
Ciruy B.Heimerdinger <ciruy.victory@gmail.com>
2021-02-13 13:27:06 +0800

    稳定版本，支持AOP前稳定版本

--- onion_view/src/main/java/com/ciruy/b/heimerdinger/onion_view/aspectj/DisposableAspect.kt
+++ onion_view/src/main/java/com/ciruy/b/heimerdinger/onion_view/aspectj/DisposableAspect.kt
@@ -1,19 +1,19 @@
-package com.ciruy.b.heimerdinger.onion_view.aspectj
-
-import android.util.Log
-import androidx.lifecycle.LifecycleOwner
-import com.ciruy.b.heimerdinger.onion_view.ext.mCompositeDisposable
-import org.aspectj.lang.annotation.Aspect
-import org.aspectj.lang.JoinPoint
-import org.aspectj.lang.annotation.After
-
-
-@Aspect
-class DisposableAspect {
-    // 需要注入的方法
-    //androidx.appcompat.app.AppCompatActivity.onDestroy
-    @After("call(* androidx.appcompat.app.AppCompatActivity.onDestroy())")
-    fun LifecycleOwner.disposeCompositeDisposable() {
-        mCompositeDisposable.dispose()
-    }
-}
+//package com.ciruy.b.heimerdinger.onion_view.aspectj
+//
+//import android.util.Log
+//import androidx.lifecycle.LifecycleOwner
+//import com.ciruy.b.heimerdinger.onion_view.ext.mCompositeDisposable
+//import org.aspectj.lang.annotation.Aspect
+//import org.aspectj.lang.JoinPoint
+//import org.aspectj.lang.annotation.After
+//
+//
+//@Aspect
+//class DisposableAspect {
+//    // 需要注入的方法
+//    //androidx.appcompat.app.AppCompatActivity.onDestroy
+//    @After("call(* androidx.appcompat.app.AppCompatActivity.onDestroy())")
+//    fun LifecycleOwner.disposeCompositeDisposable() {
+//        mCompositeDisposable.dispose()
+//    }
+//}
